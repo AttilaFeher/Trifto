a7160b6da164572ab2483e1dd44a9172a68e73a4
unknown <tutorialforhungary@gmail.com>
2024-03-23 13:38:28 +0100

    navupdate

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,17 +6,34 @@ import HeaderNav from './HeaderNav';
 import Logo from './Logo';
 import SearchBar from './SearchBar';
 import Logout from '../features/authentication/Logout';
+import { FaRegUserCircle } from 'react-icons/fa';
+import { BsFillHeartFill } from 'react-icons/bs';
+import { FaAngleDown } from 'react-icons/fa';
+import { MdMail } from 'react-icons/md';
 
 function Header() {
   const { isAuthenticated } = useUser();
 
   return (
     <>
-      <header className="mx-auto flex h-24 max-w-[1600px] items-center justify-between  px-4">
+      <header className="mx-[160px] flex h-24 items-center justify-between  px-4">
         <Logo />
         <SearchBar />
         {isAuthenticated ? (
-          <Logout />
+          <div className="flex flex-row items-center gap-4">
+            <Button variation="primary">Postavite oglas</Button>
+            <BsFillHeartFill className="h-[28px] w-[28px] cursor-pointer hover:text-red-500" />
+            <MdMail className="h-[34px] w-[34px] cursor-pointer hover:text-green-300" />
+
+            <div
+              className="flex cursor-pointer items-center gap-0"
+              id="#hover-container "
+            >
+              <FaRegUserCircle className="h-[32px] w-[32px] " />
+              <FaAngleDown className="h-[16px] w-[16px]" />
+            </div>
+            <Logout></Logout>
+          </div>
         ) : (
           <ButtonGroup>
             <Button variation="secondary">
@@ -28,7 +45,6 @@ function Header() {
           </ButtonGroup>
         )}
       </header>
-      <HeaderNav />
     </>
   );
 }
